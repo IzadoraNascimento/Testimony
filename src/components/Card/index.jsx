6790daf4c24ac37c6ac459347bc8c8f146996871
IzadoraNascimento <izasnascimento@icloud.com>
2023-07-24 23:12:24 -0300

    responsividade aplicada

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -6,16 +6,16 @@ export default function Card({image, name, subtitle, prevCard, nextCard}){
   return (
     <div className={styles.container}>
       <div className={styles.row}>
-        <div className='element'>
+        <div className={styles.element}>
           <img className={styles.image} src={image}/>
         </div>
-        <div className='element'>
+        <div className={styles.element}>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, ullam exercitationem sed a, dolorem cupiditate ut aut doloremque quos voluptates eius. Hic adipisci eos quo eum mollitia rerum repudiandae? Optio.</p>
           <h2>{name}</h2>
-          <span>{subtitle}</span>
+          <h3>{subtitle}</h3>
         </div>
       </div>
-      <div className='element'>
+      <div className={styles.element}>
         <Icon prevCard={prevCard} nextCard={nextCard} />
       </div>
     </div>
